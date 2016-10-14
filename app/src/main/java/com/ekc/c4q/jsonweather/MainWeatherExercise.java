00324b44e7373a563208563dc01d284bbd798909
Erick Chang <ekc.dev@gmail.com>
2016-10-14 02:11:52 -0400

    JsonValidator and MainWeatherExercise

---
+++ app/src/main/java/com/ekc/c4q/jsonweather/MainWeatherExercise.java
@@ -0,0 +1,19 @@
+package com.ekc.c4q.jsonweather;
+
+public class MainWeatherExercise {
+  private static String SIMPLE = "./app/src/main/assets/coord.json";
+  private static String NORMAL = "./app/src/main/assets/city.json";
+  private static String MEDIUM = "./app/src/main/assets/forecast.json";
+  private static String WTF = "./app/src/main/assets/five_day_forecast.json";
+
+  public static void main(String[] args) {
+    // Instructions for Problems 1-4:
+    // Replace MyClass with your class name.
+    // Execute the validate function and view console for Passed or Failed
+    //
+    //JsonValidator.validate(MyClass.class, SIMPLE);
+    //JsonValidator.validate(MyClass.class, NORMAL);
+    //JsonValidator.validate(MyClass.class, MEDIUM); // hint, you probably want MyClass[].class
+    //JsonValidator.validate(MyClass.class, WTF);
+  }
+}
---
+++ app/src/main/java/com/ekc/c4q/jsonweather/internal/JsonValidator.java
@@ -0,0 +1,83 @@
+package com.ekc.c4q.jsonweather.internal;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+
+/**
+ * INTERNAL USE ONLY. If you are a student, try not to touch this class kthx.
+ * Validates in class exercise java objects against the respective json.
+ */
+public class JsonValidator {
+  private static final Gson GSON = new Gson();
+
+  /**
+   * Naive validator who assumes any field that is a default value is invalid. So please use json
+   * examples that have non-default values in fields. The following default values will lead to
+   * returning a false negative:<ul>
+   * <li>int: 0
+   * <li>boolean: false
+   * <li>float: 0.0F
+   * <li>double: 0.0D
+   * <li>long: 0L
+   * <li>Object: null
+   * </ul>
+   * Validator will handle array objects correctly and recursively validate inner objects.
+   */
+  public static <T> boolean validate(Class<?> clazz, String filepath) {
+    System.out.println(String.format("Validating %s", clazz.getSimpleName()));
+    try {
+      JsonReader jsonReader = new JsonReader(new FileReader(filepath));
+      final T pojo = GSON.fromJson(jsonReader, clazz);
+      return validate(clazz, pojo);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+      return failed();
+    }
+  }
+
+  private static <T> boolean validate(Class<?> clazz, T pojo) {
+    if (clazz.isArray()) {
+      System.out.println("Array found, validating elements...");
+      clazz = clazz.getComponentType();
+      for (int i = 0; i < Array.getLength(pojo); i++) {
+        System.out.println(String.format("Checking element %s...", i));
+        if (!validate(clazz, Array.get(pojo, i))) {
+          return false;
+        }
+      }
+    } else {
+      try {
+        for (Field f : clazz.getDeclaredFields()) {
+          f.setAccessible(true);
+          Class type = f.getType();
+          Object value = f.get(pojo);
+          if (type == boolean.class && Boolean.FALSE.equals(value)) {
+            return failed();
+          } else if (type.isPrimitive() && ((Number) value).doubleValue() == 0) {
+            return failed();
+          } else if (!type.isPrimitive() && value == null) {
+            return failed();
+          }
+        }
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+        return failed();
+      }
+    }
+    return passed();
+  }
+
+  private static boolean passed() {
+    System.out.println("Passed!");
+    return true;
+  }
+
+  private static boolean failed() {
+    System.out.println("Failed!");
+    return false;
+  }
+}
