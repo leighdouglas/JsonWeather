daa99186f8eca1891611237640338b5fe0441544
Erick Chang <ekc.dev@gmail.com>
2016-10-14 02:15:35 -0400

    WTF solution

---
+++ app/src/main/java/com/ekc/c4q/jsonweather/model/FiveDayForecast.java
@@ -0,0 +1,11 @@
+package com.ekc.c4q.jsonweather.model;
+
+import java.util.List;
+
+public class FiveDayForecast {
+  City city;
+  String cod;
+  float message;
+  int cnt;
+  List<Forecast> list;
+}
